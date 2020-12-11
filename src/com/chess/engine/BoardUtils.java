db2ace803c03e9a9653d86435bc441dc2a644393
Gloria Sun <gloriasun4@gmail.com>
2020-12-10 22:15:52 -0500

    Created authentic Pawn class - missing en passant Move

--- src/com/chess/engine/BoardUtils.java
+++ src/com/chess/engine/BoardUtils.java
@@ -7,6 +7,11 @@ public class BoardUtils {
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
+    public static final boolean[] FIRST_ROW = initRow(0);
+    public static final boolean[] SECOND_ROW = initRow(1);
+    public static final boolean[] SEVENTH_ROW = initRow(6);
+    public static final boolean[] EIGHTH_ROW = initRow(7);
+
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
@@ -21,6 +26,14 @@ public class BoardUtils {
             columnNumber +=NUM_TILES_PER_ROW;
         } while(columnNumber <64);
         return column;
+    }
+
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[NUM_TILES];
+        for (int i = rowNumber; i < NUM_TILES_PER_ROW; i++) {
+            row[i] = true;
+        }
+        return row;
     }
 
     public static boolean isValidTileCoordinate(final int coordinate) {
--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -1,8 +1,12 @@
 package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
+import com.chess.engine.BoardUtils;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+import static com.chess.engine.board.Move.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,7 +14,7 @@ import java.util.List;
 
 public class Pawn extends Piece{
 
-    private static final int[] CANDIDATE_MOVES = {-8, 8};
+    //private static final int[] CANDIDATE_MOVES = {-8, 8};
 
     Pawn(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -18,9 +22,80 @@ public class Pawn extends Piece{
 
     @Override
     public Collection<Move> calculateLegalMoves(final Board board) {
+    /* assume = tiles 0-63 orientation is white on bottom, black on top
+    if piece is white,
+    legal moves = up (if not occupied) -8, add move of promotion on 1st row
+    , up+left (-9) and up+right (-7) (only if occupied by enemy piece, create attack move)
+    up 2 spaces (-16) if on 7th row
+
+
+    if piece is black,
+    legal moves = down (if not occupied) 8, add move of promotion on 8th row
+    down+left (7) and down+right (9) (only if occupied by enemy piece, create attack move)
+    down 2 spaces (16) if on 2nd row
+
+    en passant??? only if last move was 2 spaces up, need way to track last move
+     */
 
         final List<Move> legalMoves = new ArrayList<>();
-        return null;
+        int candidateDestinationCoordinate = this.piecePosition;
+        Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+        if (this.pieceAlliance == Alliance.WHITE) {
+
+            candidateDestinationCoordinate = this.piecePosition - 8;
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                        !candidateDestinationTile.isTileOccupied()) {
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
+
+            candidateDestinationCoordinate = this.piecePosition - 16;
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                        BoardUtils.SEVENTH_ROW[this.piecePosition] && !candidateDestinationTile.isTileOccupied()) {
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+            }
+
+            candidateDestinationCoordinate = this.piecePosition - 7;
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                    candidateDestinationTile.isTileOccupied() && this.pieceAlliance != pieceAlliance) {
+                legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+            }
+
+            candidateDestinationCoordinate = this.piecePosition - 9;
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                    candidateDestinationTile.isTileOccupied() && this.pieceAlliance != pieceAlliance) {
+                legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+            }
+        }
+        else {
+            candidateDestinationCoordinate = this.piecePosition + 8;
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                    !candidateDestinationTile.isTileOccupied()) {
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+            }
+
+            candidateDestinationCoordinate = this.piecePosition + 16;
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                    BoardUtils.SECOND_ROW[this.piecePosition] && !candidateDestinationTile.isTileOccupied()) {
+                legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+            }
+
+            candidateDestinationCoordinate = this.piecePosition + 7;
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                    candidateDestinationTile.isTileOccupied() && this.pieceAlliance != pieceAlliance) {
+                legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+            }
+
+            candidateDestinationCoordinate = this.piecePosition + 9;
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate) &&
+                    candidateDestinationTile.isTileOccupied() && this.pieceAlliance != pieceAlliance) {
+                legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
 
     }
 }
