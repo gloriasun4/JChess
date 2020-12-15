6c3a8b526a4da5e85a0ea50d81e19f5d294721e3
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2020-12-15 15:46:27 -0500

    Board

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,5 +1,10 @@
 package com.chess.engine.board;
 
+import com.chess.engine.Alliance;
+import com.chess.engine.pieces.Piece;
+
+import java.util.Map;
+
 public class Board {
 
     private Board (Builder builder) {
@@ -11,6 +16,17 @@ public class Board {
 
     public static class Builder {
 
+        Map<Integer, Piece> boardConfig;
+        Alliance nextMoveMaker;
+
+        public Builder() {
+
+        }
+
+        public Builder setPiece(final Piece piece) {
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
         public Board build() {
             return new Board(this);
         }
