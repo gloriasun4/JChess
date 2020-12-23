3389ef5de8d5fc4e72492658d93daf65ce27d8da
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2020-12-23 11:53:18 -0500

    Player (Part III)

--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -5,20 +5,36 @@ import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.King;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public abstract class Player {
 
     protected final Board board;
     protected final King playerKing;
     protected final Collection<Move> legalMoves;
+    private final boolean isInCheck;
 
     Player(final Board board, final Collection<Move> legalMoves, final Collection<Move> opponentMoves) {
 
         this.board = board;
         this.playerKing = establishKing();
         this.legalMoves = legalMoves;
+        this.isInCheck = !Player.calculateAttacksOnTile(this.playerKing.getPiecePosition(), opponentMoves).isEmpty();
+    }
+
+    private static Collection<Move>
+    calculateAttacksOnTile(int piecePosition, Collection<Move> moves) {
+        final List<Move> attackMoves = new ArrayList<>();
+        for(final Move move : moves) {
+            if(piecePosition == move.getDestinationCoordinate()) {
+                attackMoves.add(move);
+            }
+        }
+        return ImmutableList.copyOf(attackMoves);
     }
 
     private King establishKing() {
@@ -36,14 +52,24 @@ public abstract class Player {
     }
 
     public boolean isInCheck() {
-        return false;
+        return this.isInCheck;
     }
 
     public boolean isInCheckMate() {
-        return false;
+        return this.isInCheck && !hasEscapeMoves();
     }
 
     public boolean isInStaleMate() {
+        return !this.isInCheck && !hasEscapeMoves();
+    }
+
+    protected boolean hasEscapeMoves() {
+        for(final Move move: this.legalMoves) {
+            MoveTransition transition = makeMove(move);
+            if(transition.getMoveStatus().isDone()) {
+                return true;
+            }
+        }
         return false;
     }
 
