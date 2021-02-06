60ea3e9e96d1a8a742778cce9178e1534f901e18
Gloria Sun <53097343+gloriasun4@users.noreply.github.com>
2021-02-06 17:41:45 -0500

    Connecting AI to GUI (pt. 1)

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -228,6 +228,11 @@ public class Table {
         }
     }
 
+    enum PlayerType {
+        HUMAN,
+        COMPUTER
+    }
+
     private class TilePanel extends JPanel {
         private final int tileId;
 
